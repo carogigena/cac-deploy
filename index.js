656dbf119d2f58888a1a12431cdea43e012b72f6
carogigena <130092105+carogigena@users.noreply.github.com>
2024-06-23 11:55:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res)=> {
-    res.send("CaC Deploy Prueba!!");
+    res.send("CaC Deploy Re!!");
 });
 
 const PORT = process.env.PORT || 3000;
